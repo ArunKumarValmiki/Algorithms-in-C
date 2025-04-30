079fdc986348876f347fe984ee383a5e5b74393a
ArunKumarValmiki <valmikiarunkumar5@gmail.com>
2025-04-30 14:59:50 +0530

    Add files via upload

---
+++ SearchingTechniques/ExponentialSearch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] < x)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return -1;
+}
+
+int exponentialSearch(int arr[], int n, int x) {
+    if (arr[0] == x)
+        return 0;
+    int i = 1;
+    while (i < n && arr[i] <= x)
+        i = i * 2;
+    return binarySearch(arr, i / 2, (i < n) ? i : n - 1, x);
+}
+
+int main() {
+    int n, x;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printf("Enter element to search: ");
+    scanf("%d", &x);
+    int result = exponentialSearch(arr, n, x);
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+    return 0;
+}
