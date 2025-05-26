c78e82b018de36d53241d8644d3914cc7726c0b5
ArunKumarValmiki <valmikiarunkumar5@gmail.com>
2025-05-26 21:33:40 +0530

    Create TimSort.c

---
+++ SortingTechniques/TimSort.c
@@ -0,0 +1,82 @@
+// Time Complexity 
+// Best Case     - O(n)
+// Average Case  - O(n logn) 
+// Worst Case    - O(n logn)
+
+// Space - O(n) 
+
+
+#include <stdio.h>
+#define RUN 32
+
+void insertionSort(int arr[], int left, int right) {
+    for (int i = left + 1; i <= right; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int len1 = m - l + 1, len2 = r - m;
+    int left[len1], right[len2];
+
+    for (int i = 0; i < len1; i++)
+        left[i] = arr[l + i];
+    for (int i = 0; i < len2; i++)
+        right[i] = arr[m + 1 + i];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < len1 && j < len2) {
+        if (left[i] <= right[j])
+            arr[k++] = left[i++];
+        else
+            arr[k++] = right[j++];
+    }
+
+    while (i < len1)
+        arr[k++] = left[i++];
+    while (j < len2)
+        arr[k++] = right[j++];
+}
+
+void timSort(int arr[], int n) {
+    for (int i = 0; i < n; i += RUN)
+        insertionSort(arr, i, (i + RUN - 1 < n - 1) ? (i + RUN - 1) : (n - 1));
+
+    for (int size = RUN; size < n; size = 2 * size) {
+        for (int left = 0; left < n; left += 2 * size) {
+            int mid = left + size - 1;
+            int right = (left + 2 * size - 1 < n - 1) ? (left + 2 * size - 1) : (n - 1);
+
+            if (mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    timSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
